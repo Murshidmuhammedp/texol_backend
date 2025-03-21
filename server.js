14ebe5c701491eafc5717b06a5ccead93379ec88
Murshidmuhammedp <murshid.mmp@gmail.com>
2025-03-21 09:54:12 +0530

    origin

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
+    // origin: "*",
     origin: 'https://texol.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
